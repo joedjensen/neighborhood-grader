3d77fceb817ec0f958931d3aa4034ea5e1cb3f79
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-12 10:10:56 -0700

    seat geek, and foreca api set up for calling. Added a text file of some psuedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,33 @@ function fetchResults(event) {
     }
     else {
         console.log("Thanks for the location.");
-        //move to search page
+        //move to search page or clear the page
         //then call api's
+
+        // Seatgeek api
+        fetch('https://api.seatgeek.com/2/events?venue.state=NY&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
     }
 
 }
+
+// Foreca api
+$.ajax({
+    url: 'https://fnw-us.foreca.com/api/v1/location/search/Barcelona?lang=es',
+    headers: {
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+    }
+})
+    .then(function (response) {
+        return response;
+    })
+    .then(function (data) {
+        console.log(data);
+    })
 
 searchBtnEl.on("click", fetchResults);
