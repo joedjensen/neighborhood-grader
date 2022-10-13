ce9832e6c1eb00f4b8ef7def3faa726e89ddc4bd
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-13 14:44:19 -0700

    Hooked up the entertainment api to display types and calls a function on click

--- assets/js/results.js
+++ assets/js/results.js
@@ -3,28 +3,28 @@ var resultsCardsEl = $("#results-cards")
 
 var cityArray = []
 if (localStorage.getItem('cityArray')) {
-     cityArray = JSON.parse(localStorage.getItem('cityArray'))
+    cityArray = JSON.parse(localStorage.getItem('cityArray'))
 }
 cityArray.push(cityObject)
 
-for (i=0; i<cityArray.length; i++) {
+for (i = 0; i < cityArray.length; i++) {
     console.log(cityArray[i])
     generateInfoCard(cityArray[i])
 }
 
 function generateInfoCard(cityObject) {
     console.log(resultsCardsEl[0].children)
-    var rowEl = $("<div>", {"class": "grid-x align-spaced"})
-    var outerCardEl =  $("<div>", {"class": "card cell med-10 large-10"})
-    var innerRowEl = $("<div>", {"class": "grid-x align-middle grid-margin-x grid-padding-x"})
-    var headerEl =  $("<div>", {"class": "cell medium-4 large-4"})
-    var headerCardEl =  $("<div>", {"class": "card text-center"})
-    var bodyEl = $("<div>", {"class": "cell medium-8 large-8"})
-    var bodyGridEl =  $("<div>", {"class": "grid-x align-justify grid-margin-x grid-padding-x"})
-    var bodyCardEl1 = $("<div>", {"class": "card cell medium-3 large-6 text-center", "id" : cityObject.name + "-weather-el"})
-    var bodyCardEl2 = $("<div>", {"class": "card cell medium-3 large-6 text-center", "id" : cityObject.name + "-events-el"})
-    var bodyCardEl3 = $("<div>", {"class": "card cell medium-3 large-6 text-center"})
-    var bodyCardEl4 = $("<div>", {"class": "card cell medium-3 large-6 text-center"})
+    var rowEl = $("<div>", { "class": "grid-x align-spaced" })
+    var outerCardEl = $("<div>", { "class": "card cell med-10 large-10" })
+    var innerRowEl = $("<div>", { "class": "grid-x align-middle grid-margin-x grid-padding-x" })
+    var headerEl = $("<div>", { "class": "cell medium-4 large-4" })
+    var headerCardEl = $("<div>", { "class": "card text-center" })
+    var bodyEl = $("<div>", { "class": "cell medium-8 large-8" })
+    var bodyGridEl = $("<div>", { "class": "grid-x align-justify grid-margin-x grid-padding-x" })
+    var bodyCardEl1 = $("<div>", { "class": "card cell medium-3 large-6 text-center", "id": cityObject.name + "-weather-el" })
+    var bodyCardEl2 = $("<div>", { "class": "card cell medium-3 large-6 text-center", "id": cityObject.name + "-events-el" })
+    var bodyCardEl3 = $("<div>", { "class": "card cell medium-3 large-6 text-center" })
+    var bodyCardEl4 = $("<div>", { "class": "card cell medium-3 large-6 text-center" })
     bodyCardEl1.text("Loading")
     bodyCardEl2.text("Loading")
     bodyCardEl3.text("Loading")
@@ -39,15 +39,42 @@ function generateInfoCard(cityObject) {
 }
 
 $.ajax({
-    url: 'https://api.seatgeek.com/2/events?lat=' + cityObject.lat + '&lon=' + cityObject.lon + '&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4'
+    url: 'https://api.seatgeek.com/2/events?lat=' + cityObject.lat + '&lon=' + cityObject.lon + '&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4',
+    async: 'false'
 })
-.then(function (response) {
-    cityObject['seatgeekResponse'] = response;
-    console.log(cityArray.seatgeekResponse)
-    populateEvents(cityObject)
-})
+    .then(function (response) {
+        cityObject['seatgeekResponse'] = response;
+        console.log(cityObject.seatgeekResponse)
+        populateEvents(cityObject)
+    })
 
 function populateEvents(cityObject) {
     var cardEl = $("#" + cityObject.name + "-events-el")
-    cardEl.text(cityObject.seatgeekResponse.meta.total)
+    // cardEl.text(cityObject.seatgeekResponse.meta.total)
+    cardEl.text("Entertainment")
+    var eventNumber = $("<h5>").text("Total: " + cityObject.seatgeekResponse.meta.total)
+    cardEl.append(eventNumber);
+
+    var eventTypes = [{}];
+    eventTypes.push(cityObject.seatgeekResponse.events[0].type)
+    for (let i = 0; i < cityObject.seatgeekResponse.events.length; i++) {
+        console.log(i);
+        var tempType = cityObject.seatgeekResponse.events[i].type;
+        if (!(eventTypes.includes(tempType))) {
+            console.log("Type " + tempType);
+            eventTypes.push(tempType);
+            var eventTypesEl = $("<a>").text(cityObject.seatgeekResponse.events[i].type.toUpperCase());
+            eventTypesEl.href = "#";
+            eventTypesEl.on("click", loadEventSearch);
+            cardEl.append(eventTypesEl);
+        }
+        else {
+
+        }
+    }
+    console.log(eventTypes);
+}
+
+function loadEventSearch() {
+    console.log("event Loaded");
 }
