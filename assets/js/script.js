eab255565a912698d3c5ea5ea76774af278a39f5
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-13 10:14:35 -0700

    Hooked up the api calls based off the lat/lon of the city Name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var searchBtnEl = $(".searchBtn");
 
+//Need to sync up the locations 
+var lat = 0;
+var lon = 0;
+
 function fetchResults(event) {
     event.preventDefault();
     //save street and city values
@@ -11,34 +15,86 @@ function fetchResults(event) {
         return;
     }
     else {
-        console.log("Thanks for the location.");
+        // console.log("Thanks for the location.");
         //move to search page or clear the page
         //then call api's
 
-        // Seatgeek api
-        fetch('https://api.seatgeek.com/2/events?venue.state=NY&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4')
+
+
+
+        // Foreca api grab lat/lon by city name
+        $.ajax({
+            url: 'https://fnw-us.foreca.com/api/v1/location/search/' + cityName + '?lang=en',
+            async: 'false',
+            headers: {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+            }
+        })
             .then(function (response) {
-                return response.json();
+                return response;
             })
             .then(function (data) {
                 console.log(data);
+                console.log("Lets get this lat/lon");
+
+                lon = data.locations[0].lon;
+                lat = data.locations[0].lat;
+
+                lon = parseFloat(lon).toFixed(2);
+                lat = parseFloat(lat).toFixed(2);
+
+                console.log(lon + " this is lon");
+                console.log(lat + " this is lat");
+
+                // Seatgeek api
+                // fetch('https://api.seatgeek.com/2/events?venue.state=NY&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4')
+                fetch('https://api.seatgeek.com/2/events?lat=' + lat + '&lon=' + lon + '&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4')
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+                        console.log("Events");
+                    })
+
+                // // Foreca api
+                //this call gets the day 
+                // $.ajax({
+                //     type: 'GET',
+                //     async: 'false',
+                //     url: 'https://fnw-us.foreca.com/api/v1/observation/latest/%22' + lon + ',' + lat + '%22?lang=en&tempunit=F',
+                //     headers: {
+                //         "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+                //     }
+                // })
+                //     .then(function (response) {
+                //         return response;
+                //     })
+                //     .then(function (data) {
+                //         console.log(data);
+
+                //     })
             })
+
+
+
+
     }
 
 }
 
 // Foreca api
-$.ajax({
-    url: 'https://fnw-us.foreca.com/api/v1/location/search/Barcelona?lang=es',
-    headers: {
-        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
-    }
-})
-    .then(function (response) {
-        return response;
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+// $.ajax({
+//     url: 'https://fnw-us.foreca.com/api/v1/location/search/Barcelona?lang=es',
+//     headers: {
+//         "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+//     }
+// })
+//     .then(function (response) {
+//         return response;
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     })
 
 searchBtnEl.on("click", fetchResults);
