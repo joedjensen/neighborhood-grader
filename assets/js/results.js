49ee72af16e6752a6582bf9cddb55318b82bfee6
josephjensen <joe@cryptonumerics.com>
2022-10-13 15:22:08 -0700

    add search modal functionality and close buttons

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,5 +1,6 @@
 var cityObject = JSON.parse(localStorage.getItem("cityObject"))
 var resultsCardsEl = $("#results-cards")
+var searchBtnEl = $(".searchBtn");
 
 var cityArray = []
 if (localStorage.getItem('cityArray')) {
@@ -18,7 +19,9 @@ for (i = 0; i < cityArray.length; i++) {
 
 function generateInfoCard(cityObject) {
     console.log(resultsCardsEl[0].children)
-    var largeCardEl = $("<div>", {"class": "cell card align-center auto"})
+    var largeCardEl = $("<div>", {"class": "cell card align-center auto result-card"})
+    var closeButtonEl =$("<button>", {"class": "close-button close-btn", "aria-label":"Close alert", "type":"button"})
+    closeButtonEl.html("<span>&times;</span>")
     var cardGridX =  $("<div>", {"class": "grid-x grid-padding-x grid-margin-x"})
     var headerGridY = $("<div>", {"class": "grid-y align-center", "style":"height:100%"})
     var headerEl =  $("<div>", {"class": "cell medium-4 large-4"})
@@ -36,6 +39,7 @@ function generateInfoCard(cityObject) {
     headerEl.append(headerGridY.append(headerCardEl))
     headerCardEl.text("Loading")
     largeCardEl.append(cardGridX.append(headerEl).append(bodyEl))
+    largeCardEl.append(closeButtonEl)
     resultsCardsEl.append(largeCardEl)
     console.log(resultsCardsEl[0].children)
 }
@@ -75,7 +79,6 @@ function populateEvents(cityObject) {
     }
     console.log(eventTypes);
 }
-
 
 function weatherApi(cityObject) {
 
@@ -143,4 +146,89 @@ function populateJobs(cityObject) {
             jobsList.append(jobEx1, jobEx2, jobEx3);
             cardEl.append(jobsList);
         })
+
+    }
+
+function fetchResults(event) {
+    event.preventDefault();
+    localStorage.setItem('cityArray', JSON.stringify(cityArray))
+    //save street and city values
+    var streetName = $("#findtext").val();
+    var cityName = $("#findlocate").val();
+
+    if (!streetName || !cityName) {
+        console.log("Need both street and city.");
+        return;
+    }
+    else {
+        // console.log("Thanks for the location.");
+        //move to search page or clear the page
+        //then call api's
+
+
+
+
+        // Foreca api grab lat/lon by city name
+        $.ajax({
+            url: 'https://fnw-us.foreca.com/api/v1/location/search/' + cityName + '?lang=en',
+            async: 'false',
+            headers: {
+                "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+            }
+        })
+            .then(function (response) {
+                return response;
+            })
+            .then(function (data) {
+                console.log(data);
+                console.log("Lets get this lat/lon");
+
+                lon = data.locations[0].lon;
+                lat = data.locations[0].lat;
+
+                lon = parseFloat(lon).toFixed(2);
+                lat = parseFloat(lat).toFixed(2);
+
+                console.log(lon + " this is lon");
+                console.log(lat + " this is lat");
+                var cityObject = {
+                    "name": cityName,
+                    "lat":lat,
+                    "lon":lon
+                }
+                localStorage.setItem("cityObject", JSON.stringify(cityObject));
+                // Seatgeek api
+                // fetch('https://api.seatgeek.com/2/events?venue.state=NY&client_id=Mjk2NTg1OTB8MTY2NTUxOTc2Ny4yMjYwMDQ4')
+                document.location = "./results.html"
+
+                // // Foreca api
+                //this call gets the day 
+                // $.ajax({
+                //     type: 'GET',
+                //     async: 'false',
+                //     url: 'https://fnw-us.foreca.com/api/v1/observation/latest/%22' + lon + ',' + lat + '%22?lang=en&tempunit=F',
+                //     headers: {
+                //         "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2NTUyNjI0NiwiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjU1MjYyNDYsImp0aSI6IjI3Yzc0MGYzODg0ODEwZWMiLCJzdWIiOiJqb2huZnJvbTIwOSIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.R6lwtfrLoCrBhdTpLpZaeKjDbjeQWfZmla6i759u7Wg",
+                //     }
+                // })
+                //     .then(function (response) {
+                //         return response;
+                //     })
+                //     .then(function (data) {
+                //         console.log(data);
+
+                //     })
+            })
+
+
+
+    }
+
+}
+
+searchBtnEl.on("click", fetchResults);
+resultsCardsEl.on("click","button", removeCity)
+
+function removeCity() {
+    document.location = "./index.html"
 }
